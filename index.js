acea2aa6147148ed6f0d2eafc4afe260a8a27491
Alex Marshall <alex.k.marshall83@gmail.com>
2020-05-23 13:46:03 +0100

    Add script solution for full width backgrounds

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const root = document.documentElement;
 const body = document.getElementsByTagName("body")[0];
+const fullWidthBackground = document.getElementsByClassName(
+  "full-width-background"
+)[0];
+
 setVisibleWidth();
+setLeftOffset();
 
 window.addEventListener("resize", () => {
   setVisibleWidth();
+  setLeftOffset();
 });
 
 function setVisibleWidth() {
   let bodyWidth = body.offsetWidth;
   root.style.setProperty("--visible-width", `${bodyWidth}px`);
 }
+
+function setLeftOffset() {
+  const leftOffset = -1 * fullWidthBackground.getBoundingClientRect().left;
+  root.style.setProperty("--left-offset", `${leftOffset}px`);
+}
+
+const heroInfo = document.getElementsByClassName("hero-info")[0];
+
+console.log(heroInfo.getBoundingClientRect());
